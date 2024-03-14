3e3d30202d71d0a180153e258cb370e833d50a86
taznimt <taznimanwar06@gmail.com>
2024-03-14 14:46:31 +0700

    membuat step 3

--- pertemuanke4/pertemuanke4.cpp
+++ pertemuanke4/pertemuanke4.cpp
@@ -11,7 +11,12 @@ void InsertionSort() {
     for (i = 1; i <= n - 1; i++) { //step 1
         temp = arr[i]; // step 2
 
+        j = i - 1; //step 3
+
         while (j >= 0 && arr[j] > temp) // step 4
+        {
+            
+        }
 
     }
 }

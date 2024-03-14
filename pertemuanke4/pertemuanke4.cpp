30a96229fbf5cfca362ba7ca3c3642dd06d390fa
taznimt <taznimanwar06@gmail.com>
2024-03-14 14:36:11 +0700

    Add project files.

---
+++ pertemuanke4/pertemuanke4.cpp
@@ -0,0 +1,19 @@
+// pertemuanke4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+void InsertionSort() {
+    int i, j, n, temp;
+    int arr[20];
+
+    for (i = 1; i <= n - 1; i++) { //step 1
+
+    }
+}
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+

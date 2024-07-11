5f00300574bf7e99548a512b4972dbcc80237fcb
taznimt <taznimanwar06@gmail.com>
2024-07-11 11:35:24 +0700

    create insertion short, display dan memanggil fungsi utama

--- pertemuanke4/pertemuanke4.cpp
+++ pertemuanke4/pertemuanke4.cpp
@@ -4,9 +4,36 @@
 #include <iostream>
 using namespace std;
 
+int n; 
+int arr[20];
+
+void input() {
+    while (true) {
+        cout << "masukan banyaknya elemen pada array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+
+        }
+    }
+    cout << endl;
+    cout << "==================" << endl;
+    cout << "masukan Elemen Array" << endl;
+    cout << "==================" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+
+    }
+
+}
+
 void InsertionSort() {
-    int i, j, n, temp;
-    int arr[20];
+    int i, j, temp;
+    
 
     for (i = 1; i <= n - 1; i++) { //step 1
         temp = arr[i]; // step 2
@@ -15,15 +42,44 @@ void InsertionSort() {
 
         while (j >= 0 && arr[j] > temp) // step 4
         {
-            arr[j] = arr[j + 1]; //step 4a
+            arr[j+1] = arr[j ]; //step 4a
             j--; //step 4b
 
         }
         arr[j + 1] = temp; //step 5
     }
 }
+
+    void display() {
+        cout << endl;
+        cout << "=============" << endl;
+        cout << "Element Array yang telah tersusun" << endl;
+        cout << "=============" << endl;
+        for (int j = 0; j < n; j++) {
+            cout << arr[j] << endl; //output setiap element array pada garis baru
+            if (j < n - 1) {
+                cout << " --> ";
+            }
+        }
+        cout << "jumalah pass = " << n - 1 << endl; // menampilkan total angka dari elemen yang benar
+        cout << endl;
+        cout << endl;
+        cout << "==================" << endl;
+        cout << "masukan Elemen Array" << endl;
+        cout << "==================" << endl;
+
+        
+
+    }
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    input(); 
+    InsertionSort(); 
+    display(); 
+    system("pause");
+
+    return 0;
 }
 
